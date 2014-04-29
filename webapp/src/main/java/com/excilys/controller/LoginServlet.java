6aad8b0467c01e5826bbc6dd2baa6b93b9e6ef16
julien.levillain <jlevillain@excilys.com>
2014-04-29 09:56:00 +0200

    used sha to encrypt on database for Spring Security

--- webapp/src/main/java/com/excilys/controller/LoginServlet.java
+++ webapp/src/main/java/com/excilys/controller/LoginServlet.java
@@ -1,10 +1,10 @@
 package com.excilys.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,22 +14,23 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping("/login")
 public class LoginServlet {
+	Logger logger = LoggerFactory.getLogger(LoginServlet.class);
 	@Autowired
 	private ApplicationContext context;
-	
-	@RequestMapping(method=RequestMethod.GET)
+
+	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView login(
 			@RequestParam(value = "error", required = false) String error,
 			@RequestParam(value = "logout", required = false) String logout) {
-	 
-			ModelAndView model = new ModelAndView();
-			if (error != null) {
-				model.addObject("error",context.getMessage("Login.error",null,  LocaleContextHolder.getLocale()));
-			}
-//			BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-//	        String hashedPassword = passwordEncoder.encode("user");
-//	        System.out.println(">>> "+hashedPassword);
-			model.setViewName("login");
-			return model;
+
+		ModelAndView model = new ModelAndView();
+		if (error != null) {
+			model.addObject(
+					"error",
+					context.getMessage("Login.error", null,
+							LocaleContextHolder.getLocale()));
 		}
+		model.setViewName("login");
+		return model;
+	}
 }
