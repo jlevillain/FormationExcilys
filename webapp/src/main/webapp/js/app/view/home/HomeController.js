0d4913cb2f064460f1f787323dde0625a1813ebb
Julien Levillain <jlevillain@ecole.ensicaen.fr>
2015-02-21 08:26:12 +0100

    ajout confirmation pour supprimer un ordinateur

--- webapp/src/main/webapp/js/app/view/home/HomeController.js
+++ webapp/src/main/webapp/js/app/view/home/HomeController.js
@@ -5,6 +5,10 @@ Ext.define('MyApp.view.home.HomeController', {
     extend: 'Ext.app.ViewController',
     alias: 'controller.home',
     deletedText:' deleted',
+    confirm:'Confirm',
+    confirmQuestion:'Are you sure?',
+    rowIndex:-1,
+    grid:null,
     init:function() {
         console.log('init home')
     },
@@ -12,10 +16,20 @@ Ext.define('MyApp.view.home.HomeController', {
         this.redirectTo('user/'+LOCALE);
     },
     DeleteComputer:function(grid, rowIndex, colIndex) {
-        var store = grid.getStore();
-        var rec = store.getAt(rowIndex);
-        rec.erase({success:function() {
-            alert(rec.get('name')+this.deletedText);
-        }});
+        this.rowIndex = rowIndex;
+        this.grid = grid;
+        Ext.Msg.confirm(this.confirm, this.confirmQuestion, 'onConfirm', this);
+
+    },
+
+    onConfirm: function (choice) {
+        me = this;
+        if (choice === 'yes') {
+            var store = this.grid.getStore();
+            var rec = store.getAt(this.rowIndex);
+            rec.erase({success:function() {
+                alert(rec.get('name')+me.deletedText);
+            }});
+        }
     }
 });
